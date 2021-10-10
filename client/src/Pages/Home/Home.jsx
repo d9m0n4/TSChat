11d0e0d1b76a5b9a528bbf897e36e251ef271524
c2h8e9d4@gmail.com <c2h8e9d4@gmail.com>
2021-10-10 22:48:16 +0300

    main page chats block

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { Avatar } from 'antd';
+
 import { Input } from 'antd';
 import './index.scss';
 
@@ -8,17 +10,15 @@ import messageIcon from '../../assets/img/icons/message-circle.svg';
 import bookmarkIcon from '../../assets/img/icons/bookmark.svg';
 import settingsIcon from '../../assets/img/icons/settings.svg';
 import logOutIcon from '../../assets/img/icons/log-out.svg';
-
-import Avatar from '../../assets/img/Group18.png';
+import controlsIcon from '../../assets/img/icons/controls.svg';
+import plusIcon from '../../assets/img/icons/plus.svg';
 
 const Home = () => {
   return (
     <section className="home-page">
       <div className="sidebar">
         <div className="user__profile-link">
-          <div className="user__avatar">
-            <img src={Avatar} alt="" />
-          </div>
+          <Avatar size={48}>Д </Avatar>
         </div>
         <nav className="sidebar__navigation">
           <ul className="nav-group">
@@ -48,12 +48,191 @@ const Home = () => {
         </nav>
       </div>
       <div className="main__content">
-        <div className="top-bar">
-          <div className="top-bar__search">
-            <Input />
+        <div className="main__content-body">
+          <div className="main__content-body__leftbar">
+            <div className="leftbar__header">
+              <div className="top-bar__search">
+                <Input placeholder="Поиск разговоров..." />
+              </div>
+            </div>
+            <div className="leftbar__body">
+              <div className="leftbar__chats">
+                <div className="leftbar__chats-header">
+                  <div className="chats__title">Диалоги</div>
+                  <div className="chats__control">
+                    <a href="#">
+                      <img src={plusIcon} alt="" />
+                    </a>
+                  </div>
+                </div>
+                <div className="leftbar__chats-body">
+                  <div className="chats__item">
+                    <div className="chats__item-avatar">
+                      <Avatar
+                        size={40}
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+                      />
+                    </div>
+                    <div className="chats__item-body">
+                      <div className="chats__item-top">
+                        <p className="item-name">Александр Пушкин</p>
+                        <span className="item-date">21.04.2021</span>
+                      </div>
+                      <div className="chats__item-bottom">
+                        <div className="item__message">Шо как мужуки??? Есть че...</div>
+                        <div className="item__status"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="chats__item">
+                    <div className="chats__item-avatar">
+                      <Avatar
+                        size={40}
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+                      />
+                    </div>
+                    <div className="chats__item-body">
+                      <div className="chats__item-top">
+                        <p className="item-name">Александр Пушкин</p>
+                        <span className="item-date">21.04.2021</span>
+                      </div>
+                      <div className="chats__item-bottom">
+                        <div className="item__message">Шо как мужуки??? Есть че...</div>
+                        <div className="item__status"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="chats__item">
+                    <div className="chats__item-avatar">
+                      <Avatar
+                        size={40}
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+                      />
+                    </div>
+                    <div className="chats__item-body">
+                      <div className="chats__item-top">
+                        <p className="item-name">Александр Пушкин</p>
+                        <span className="item-date">21.04.2021</span>
+                      </div>
+                      <div className="chats__item-bottom">
+                        <div className="item__message">Шо как мужуки??? Есть че...</div>
+                        <div className="item__status"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="chats__item">
+                    <div className="chats__item-avatar">
+                      <Avatar
+                        size={40}
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+                      />
+                    </div>
+                    <div className="chats__item-body">
+                      <div className="chats__item-top">
+                        <p className="item-name">Александр Пушкин</p>
+                        <span className="item-date">21.04.2021</span>
+                      </div>
+                      <div className="chats__item-bottom">
+                        <div className="item__message">Шо как мужуки??? Есть че...</div>
+                        <div className="item__status"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="leftbar__chats">
+                <div className="leftbar__chats-header">
+                  <div className="chats__title">Беседы</div>
+                  <div className="chats__control">
+                    <a href="#">
+                      <img src={plusIcon} alt="" />
+                    </a>
+                  </div>
+                </div>
+                <div className="leftbar__chats-body">
+                  <div className="chats__item">
+                    <div className="chats__item-avatar">
+                      <Avatar size={40}> Б </Avatar>
+                    </div>
+                    <div className="chats__item-body">
+                      <div className="chats__item-top">
+                        <p className="item-name">Бухгалтерия</p>
+                        <span className="item-date">21.04.2021</span>
+                      </div>
+                      <div className="chats__item-bottom conv">
+                        <div className="conv__sender">Экономист:</div>
+                        <div className="conv__message">Шо как мужуки???</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="chats__item">
+                    <div className="chats__item-avatar">
+                      <Avatar size={40}> Б </Avatar>
+                    </div>
+                    <div className="chats__item-body">
+                      <div className="chats__item-top">
+                        <p className="item-name">Бухгалтерия</p>
+                        <span className="item-date">21.04.2021</span>
+                      </div>
+                      <div className="chats__item-bottom conv">
+                        <div className="conv__sender">Экономист:</div>
+                        <div className="conv__message">Шо как мужуки???</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="chats__item">
+                    <div className="chats__item-avatar">
+                      <Avatar size={40}> Б </Avatar>
+                    </div>
+                    <div className="chats__item-body">
+                      <div className="chats__item-top">
+                        <p className="item-name">Бухгалтерия</p>
+                        <span className="item-date">21.04.2021</span>
+                      </div>
+                      <div className="chats__item-bottom conv">
+                        <div className="conv__sender">Экономист:</div>
+                        <div className="conv__message">Шо как мужуки???</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="chats__item">
+                    <div className="chats__item-avatar">
+                      <Avatar size={40}> Б </Avatar>
+                    </div>
+                    <div className="chats__item-body">
+                      <div className="chats__item-top">
+                        <p className="item-name">Бухгалтерия</p>
+                        <span className="item-date">21.04.2021</span>
+                      </div>
+                      <div className="chats__item-bottom conv">
+                        <div className="conv__sender">Экономист:</div>
+                        <div className="conv__message">Шо как мужуки???</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="chats__item">
+                    <div className="chats__item-avatar">
+                      <Avatar size={40}> Б </Avatar>
+                    </div>
+                    <div className="chats__item-body">
+                      <div className="chats__item-top">
+                        <p className="item-name">Бухгалтерия</p>
+                        <span className="item-date">21.04.2021</span>
+                      </div>
+                      <div className="chats__item-bottom conv">
+                        <div className="conv__sender">Экономист:</div>
+                        <div className="conv__message">Шо как мужуки???</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="top-bar__"></div>
-          <div className="top-bar__controls"></div>
+          <div className="main__content-body__messages">qwe</div>
+          <div className="main__content-body__rightbar">
+            <div className="rightbar__chat-info">123</div>
+          </div>
         </div>
       </div>
     </section>
