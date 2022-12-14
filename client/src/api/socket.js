be0c788eb66aee7dda2d1127275c555b05b78835
Дмитрий <59685224+d9m0n4@users.noreply.github.com>
2022-12-14 09:34:04 +0300

    Update socket.js

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const socket = io('https://tschat-production.up.railway.app/api', {
+const socket = io('https://tschat-production.up.railway.app', {
 withCredentials: true});
 
 export default socket;
